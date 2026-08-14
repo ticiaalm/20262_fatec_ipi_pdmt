3ff62d60d82145bb01cb209bba8cbe87b6139222
Letícia Almeida <leticiaalmeidadev@gmail.com>
2026-08-14 18:17:51 -0300

    learn(ap04-react): instala e testa bootstrap e ícones
    - retomar montando o esquema de responsividade
    - fazer a lista com quatro pedidos

---
+++ ap04-react-props/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="container border">
+      <div className="row">
+        <div className="col-12">
+          <i className="fa-solid fa-hippo"></i>
+        </div>
+      </div>
+  </div>
+  )
+}
+
+export default App

16276ae0128038f7d1d7768a46318d6d8218f83f
Letícia Almeida <leticiaalmeidadev@gmail.com>
2026-09-04 15:39:48 -0300

    learn(ap05-react): encerra a apostila

--- ap05-react-estado/src/App.jsx
+++ ap05-react-estado/src/App.jsx
@@ -7,44 +7,48 @@ class App extends React.Component {
             longitude: null,
             estacao: null,
             data: null,
-            icone: null
+            icone: null,
+            mensagemDeErro: null
         }
     }
     render() {
         return (
-        <div className='container border mt-2 py-3'>
-            <div className='row justify-content-center'>
-                <div className='col-12 col-md-8'>
-                    <div className='card'>
-                        <div className='card-body'>
-                            <div 
-                            style={{height: '6rem'}}
-                            className='d-flex align-items-center border rounded mb-2'>
-                                <i className={`fa-solid fa-5x ${this.state.icone}`}></i>
-                                <p className="w-75 ms-3 text-center fs-1">
-                                    {this.state.estacao}
-                                </p>
+            <div className='container border mt-2 py-3'>
+                <div className='row justify-content-center'>
+                    <div className='col-12 col-md-8'>
+                        <div className='card'>
+                            <div className='card-body'>
+                                <div
+                                    style={{ height: '6rem' }}
+                                    className='d-flex align-items-center border rounded mb-2'>
+                                    <i className={`fa-solid fa-5x ${this.state.icone}`}></i>
+                                    <p className="w-75 ms-3 text-center fs-1">
+                                        {this.state.estacao}
+                                    </p>
+                                </div>
+                                <div>
+                                    <p className="text-center">
+                                        {
+                                            this.state.latitude ?
+                                                `Coordenadas: ${this.state.latitude}, ${this.state.longitude} | Data: ${this.state.data}`
+                                                :
+                                                this.state.mensagemDeErro ?
+                                                `Tente novamente mais tarde`
+                                                :
+                                                `Clique no botão para saber a sua estação climática`
+                                        }
+                                    </p>
+                                </div>
+                                <button
+                                    className='btn btn-outline-primary w-100 mt-2'
+                                    onClick={this.obterLocalizacao}>
+                                    Qual a minha estação?
+                                </button>
                             </div>
-                            <div>
-                                <p className="text-center">
-                                    {
-                                        this.state.latitude ?
-                                        `Coordenadas: ${this.state.latitude}, ${this.state.longitude} | Data: ${this.state.data}`
-                                        :
-                                        'Clique no botão para saber a sua estação climática'
-                                    }
-                                </p>
-                            </div>
-                            <button
-                            className='btn btn-outline-primary w-100 mt-2'
-                            onClick={this.obterLocalizacao}>
-                                Qual a minha estação?
-                            </button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         )
     }
     obterEstacao = (data, latitude) => {
@@ -88,9 +92,12 @@ class App extends React.Component {
                     data: data.toLocaleDateString(),
                     icone: icone
                 })
-            }, 
+            },
             (erro) => {
                 console.log(`Erro: ${erro}`)
+                this.setState({
+                    mensagemDeErro: 'Tente novamente mais tarde'
+                })
             }
         )
     }
